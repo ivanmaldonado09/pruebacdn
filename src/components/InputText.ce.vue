016970eece7b0f0ed7c0e5ee6e5e35823fdd2b01
Ivan <ivanm@grava.io>
2025-07-02 11:14:14 -0300

    first commit

---
+++ src/components/InputText.ce.vue
@@ -0,0 +1,21 @@
+<template>
+    <label>
+      {{ label }}
+      <input :type="type" :value="value" @input="$emit('input', $event.target.value)" />
+    </label>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      label: String,
+      type: {
+        type: String,
+        default: 'text'
+      },
+      value: String
+    },
+    emits: ['input']
+  }
+  </script>
+  
